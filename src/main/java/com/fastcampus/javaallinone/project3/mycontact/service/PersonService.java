d049c1fd8e6e828f263cbbcaac261bbff49b51cd
dpsk331 <dpsk331@naver.com>
2021-04-17 21:56:28 +0900

    JPA Relation - 3

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -1,16 +1,17 @@
 package com.fastcampus.javaallinone.project3.mycontact.service;
 
-import com.fastcampus.javaallinone.project3.mycontact.domain.Block;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
-import com.fastcampus.javaallinone.project3.mycontact.repositroy.BlockRepository;
 import com.fastcampus.javaallinone.project3.mycontact.repositroy.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class PersonService {
 
     @Autowired
@@ -20,5 +21,15 @@ public class PersonService {
         List<Person> people = personRepository.findAll();
 
         return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Person getPerson(Long id) {
+        Person person = personRepository.findById(id).get();
+
+//        System.out.println("person : " + person); // 모든 로그 출력..
+        log.info("person : {}", person); // 프로덕션에 배포가 되었을 때 로그백을 이용해 로그 출력 제한 가능
+
+        return person;
     }
 }
